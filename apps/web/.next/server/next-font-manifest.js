58d78e55caf33dfd1d844a9f0200f725d4f3bdfd
Lloyd dela Cruz <lloyd.delacruz@outlook.com>
2026-03-17 06:05:56 -0700

    deleted

--- apps/web/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/lloyd.vince1985gmail.com/Desktop/Software Dev/Apex-Protocol/apps/web/src/app/layout\":[\"static/media/13971731025ec697-s.p.woff2\",\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
